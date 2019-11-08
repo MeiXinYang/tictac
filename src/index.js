bf2b649222958083071fca2cae23294fee8e4b13
Xinyang.Mei <mxy3759035@163.com>
2019-11-08 17:49:51 +0800

    fix(*) : css 调整

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
-import {version, Button} from "antd";
+import {version, Button, Skeleton} from "antd";
 import "antd/dist/antd.css";
 import {Timeline} from 'antd';
 import "./index.css";
 import TimelineItem from "antd/es/timeline/TimelineItem";
-import Game from "./js/game"
-import Calculater from "./component/calculater"
-import ToDo from "./component/todo";
+const Game = React.lazy(() => import('./js/game'));
+const Calculater = React.lazy(() => import('./component/calculater'));
+const ToDo = React.lazy(() => import('./component/todo'));
 // ========================================
 
 ReactDOM.render(
-    <Suspense fallback={<div>fallback</div>}>
+    <Suspense fallback={<Skeleton active={true}/>}>
         <div className={"main"}>
             <Game/>
             <Calculater/>
